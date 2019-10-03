6fd8a25da42ab6a920013ce0dc62bff9d10e33e4
EC2 Default User <ec2-user@ip-172-31-38-203.us-east-2.compute.internal>
2019-10-03 20:30:54 +0000

    part 1 test5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date, :sort_by)
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
 
   def show
